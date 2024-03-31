220e42b086f1ce3f66c47a71bc73971c58480e5d
seadclark <seadclark@gmail.com>
2024-03-31 15:23:27 +0700

    old package setting

--- src/assets/js/base-url.js
+++ src/assets/js/base-url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://apiconvuemer.cybersead.com";
-export default { BASE_URL: BASE_URL }   
- 
+export const BASE_URL =
+  "http://127.0.0.1:8000/xampp-php7/htdocs/my-project/convuemer/laravue-backend";
+export default { BASE_URL: BASE_URL };
